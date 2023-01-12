cbcea05fd34a9b76111465832857acc36cba2df1
Shiva Krishna Gopannagari <34725150+shivakrishna1995@users.noreply.github.com>
2023-01-13 00:13:21 +0530

    Dev: Added react-query

--- src/app.jsx
+++ src/app.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import AddCreditCardSection from './components/AddCreditCard';
 import CreditCardsListSection from './components/CreditCardsList';
 import { Container } from 'react-bootstrap';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     return (
-        <Container 
-            fluid
-            className='p-2'>
-                <p className='fw-normal fs-4'>
-                    Credit Card System
-                </p>
-                <AddCreditCardSection />
-                <CreditCardsListSection />
-        </Container>
+        <QueryClientProvider client={queryClient}>
+            <Container 
+                fluid
+                className='p-2'>
+                    <p className='fw-normal fs-4'>
+                        Credit Card System
+                    </p>
+                    <AddCreditCardSection />
+                    <CreditCardsListSection />
+            </Container>
+        </QueryClientProvider>
     );
 };
 
